15fdceb3e6fd3b3a35383a2c08446542778303d5
Lakindu Widuranga Alwis <vidurangalakindu@gmail.com>
2023-07-31 11:02:15 +0530

    Created components for the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+// import { TestComponent } from './Component/test/test.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { HomeComponent } from './component/home/home.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { DetailsComponent } from './component/details/details.component';
+import { DetailsMoviesComponent } from './component/details-movies/details-movies.component';
+import { DetailsReviewsComponent } from './component/details-reviews/details-reviews.component';
+import { NotAuthorizedComponent } from './component/not-authorized/not-authorized.component';
+import { SearchComponent } from './component/search/search.component';
+import { ModalComponent } from './component/modal/modal.component';
+import { LoaderComponent } from './component/loader/loader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // TestComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent,
+    DetailsComponent,
+    DetailsMoviesComponent,
+    DetailsReviewsComponent,
+    NotAuthorizedComponent,
+    SearchComponent,
+    ModalComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule
