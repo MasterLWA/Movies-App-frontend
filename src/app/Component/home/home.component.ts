bce0b0b6197348b8de4de7dc39c3d9977c4b254d
Lakindu Widuranga Alwis <vidurangalakindu@gmail.com>
2023-08-15 10:37:58 +0530

    NG Styles

--- src/app/Component/home/home.component.ts
+++ src/app/Component/home/home.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  currentStyles:Record<string, string> = {}; // defined record type to store key value pair of string type
+
+  //defined boolean type variables
+  isCentered = true;
+  isSuccess = true;
+  isLarge = true;
+
+  constructor() {
+    this.currentStyles = {
+      'text-align': this.isCentered ? 'center' : 'left',
+      'color': this.isSuccess ? 'green' : 'red',
+      'font-size': this.isLarge ? '30px' : '12px'
+    }
+  }
+
 }
